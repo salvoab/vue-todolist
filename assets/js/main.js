210c5100692ea941570e1ab4facd9da29f5c199f
salvoab <salvatore.abate85@gmail.com>
2020-12-16 11:51:46 +0100

    Cartelle e file di partenza

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+Creare una todo list usando VueJS.
+
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+*/
+
+let app = new Vue({
+    el: '#root',
+    data:{
+        tasks: ['Ripassare HTML', 'Ripassare CSS', 'Approfondire JS']
+    }
+});
