50a0af062c403acc4db42b07520a720eed8ff3ab
salvoab <salvatore.abate85@gmail.com>
2020-12-17 10:46:55 +0100

    Html e css di base per nuove funzionalità del bonus
    in main.js inseriti nuovi array per nuove funzionalità

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -15,6 +15,8 @@ let app = new Vue({
     el: '#root',
     data:{
         tasks: ['Ripassare HTML', 'Ripassare CSS', 'Approfondire JS'],
+        tasksDone: [],
+        recycleBin: [],
         newTask: '',
         noTasks: false
     },
